66fad8740420e83688102d5be6c432b6fe82ae78
alexzh3 <azheng@tudelft.nl>
2025-06-03 21:09:29 +0200

    Working stable diffusion

--- flax_diffusion/flax_test_sd.py
+++ flax_diffusion/flax_test_sd.py
@@ -2,13 +2,18 @@ import jax
 import numpy as np
 from flax.jax_utils import replicate
 from flax.training.common_utils import shard
-
+import matplotlib.pyplot as plt
+import time
 from diffusers import FlaxStableDiffusionPipeline
 
-print(f"Currently using: {jax.default_backend()}")  
+# Memory management config
+jax.config.update('jax_platform_name', 'gpu')
+print(f"Default device for inference: {jax.default_backend()}")  
 
 pipeline, params = FlaxStableDiffusionPipeline.from_pretrained(
-    "CompVis/stable-diffusion-v1-4", force_download=True ,revision="bf16", dtype=jax.numpy.bfloat16
+    "CompVis/stable-diffusion-v1-4",
+    revision="bf16",
+    dtype=jax.numpy.bfloat16
 )
 
 prompt = "a photo of an astronaut riding a horse on mars"
@@ -16,7 +21,9 @@ prompt = "a photo of an astronaut riding a horse on mars"
 prng_seed = jax.random.PRNGKey(0)
 num_inference_steps = 50
 
-num_samples = jax.device_count()
+# Start timer
+start_time = time.time()
+num_samples = 1
 prompt = num_samples * [prompt]
 prompt_ids = pipeline.prepare_inputs(prompt)
 
@@ -27,3 +34,13 @@ prompt_ids = shard(prompt_ids)
 
 images = pipeline(prompt_ids, params, prng_seed, num_inference_steps, jit=True).images
 images = pipeline.numpy_to_pil(np.asarray(images.reshape((num_samples,) + images.shape[-3:])))
+# End timer
+generation_time = time.time() - start_time
+print(f"Image generation took: {generation_time:.2f} seconds")
+
+# Display the generated image
+if images:
+    plt.imshow(images[0])
+    plt.axis('off')
+    plt.show()
+    images[0].save("output.png")
